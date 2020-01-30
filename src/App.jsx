0d0a9015ba73e62d993de59723252f57fd2cf0f8
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-01-30 18:06:07 +0100

    env conf done - prettier, eslint, and conf files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  console.log('asdasd');
+
+  return (
+    <div className="App">
+      <h1>Jest</h1>
+      <hr />
+
+      <h2>przegląd pytań </h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/question/Question.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Question = () => (
+  <div>
+    <h3>No no coś tam z JS pytanie jakieś</h3>
+    <code>Odpowiedź cos cos jakas cos dana nadana</code>
+    <hr />
+  </div>
+);
+
+export default Question;
