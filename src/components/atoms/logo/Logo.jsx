04bf24d1d804327ebb2c9fcb11271073eab13e02
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-02-25 11:06:43 +0100

    add conditional topics color in answearView with switch instruction implemented directly in styled components

--- src/components/atoms/logo/Logo.jsx
+++ src/components/atoms/logo/Logo.jsx
@@ -2,10 +2,12 @@ import styled, { css } from 'styled-components';
 import logoFull from '../../../assets/images/LogoFull.svg';
 
 const Logo = styled.div`
+  display: inline-block;
   margin: 25px 0 0 25px;
   width: 450px;
   height: calc(450px / 2.7);
   background-image: url(${logoFull});
+  /* background-color: red; */
   background-repeat: no-repeat;
   background-size: contain;
 
@@ -13,7 +15,8 @@ const Logo = styled.div`
     small &&
     css`
       width: 260px;
-      height: calc(450px / 2.7);
+      height: calc(260px / 2.7);
+      margin-bottom: 50px;
     `}
 `;
 
--- src/components/molecules/Question/Question.jsx
+++ src/components/molecules/Question/Question.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const StyledWrapper = styled.div`
   margin: 10px auto;
@@ -23,21 +23,46 @@ const StyledQuestion = styled.div`
 `;
 
 const StyledAnswer = styled.div`
+  position: relative;
   background-color: rgba(196, 196, 196, 0.2);
   width: 60%;
-  font-size: 12px;
+  font-size: 14px;
   padding: 10px;
 `;
 
 const StyledNumberFiled = styled.div`
+  position: relative;
   width: 40px;
   height: 40px;
   border-radius: 50px;
-  background-color: #eee;
+  background-color: #f9f9f9;
   margin: 0 15px 0 0;
   text-align: center;
   font-weight: bold;
   font-size: 22px;
+  z-index: 2;
+`;
+
+const StyledCategoryFiled = styled.div`
+  position: absolute;
+  width: 50px;
+  height: 29px;
+  border: 1px solid #ccc;
+  color: ${props => {
+    switch (props.topic) {
+      case 'HTML':
+        return 'red';
+      case 'JS':
+        return '#bcb649';
+      default:
+        return 'black';
+    }
+  }};
+  top: 0;
+  right: 0;
+  text-align: center;
+  font-weight: bold;
+  font-size: 16px;
 `;
 
 const Question = ({ item }) => {
@@ -52,7 +77,9 @@ const Question = ({ item }) => {
       <StyledAnswer>
         <strong>{answer}</strong>
 
-        <code>{topic}</code>
+        <StyledCategoryFiled topic={topic}>
+          <code>{topic}</code>
+        </StyledCategoryFiled>
       </StyledAnswer>
     </StyledWrapper>
   );
