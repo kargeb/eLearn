9a0ab2d632a5ce5991e870a77859685717db4c05
Karol Gebarowski <karol.gebarowski@gmail.com>
2020-03-08 08:59:19 +0100

    before refactoring Select to hook

--- src/components/atoms/input/Input.test.jsx
+++ src/components/atoms/input/Input.test.jsx
@@ -2,25 +2,43 @@ import React, { useState } from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
-it('Renders input', () => {
-  const labelText = 'Pytanie';
+// const RenderInput = props => {
+//   const [state, setState] = useState('');
+//   const labelText = 'Pytanie';
+//   const utils = render(
+//     <Input
+//       label={labelText}
+//       name="questionInput"
+//       gotValue={state}
+//       setValue={e => setState(e.target.value)}
+//       {...props}
+//     />
+//   );
 
-  //   const [state, setState] = useState('');
+//   const input = utils.getByLabelText(labelText);
 
-  const { getByLabelText } = render(
-    <Input
-      label={labelText}
-      name="questionInput"
-      //   gotValue={state}
-      //   setValue={e => e.target.value}
-    />
-  );
+//   return { ...utils, input };
+// };
 
-  const input = getByLabelText('Pytanie');
+describe('Input testing', () => {
+  //   it('input rendered test', () => {
+  //     const { input } = <RenderInput />;
+  //     expect(input).toBeInTheDocument();
+  //   });
 
-  expect(input).toBeInTheDocument();
+  it('Renders input', () => {
+    const labelText = 'Pytanie';
 
-  //   fireEvent.change(input, { target: { value: 'Pies' } });
+    const { getByLabelText } = render(
+      <Input label={labelText} name="questionInput" />
+    );
 
-  //   expect(input).toHaveValue('Pies');
+    const input = getByLabelText('Pytanie');
+    //   const showedInput = container.querySelector('#questionInput');
+    console.log(input.innerHTML);
+    expect(input).toBeInTheDocument();
+
+    //   fireEvent.change(input, { target: { value: 'Pies' } });
+    //   expect(input).toHaveValue('Piess');
+  });
 });
